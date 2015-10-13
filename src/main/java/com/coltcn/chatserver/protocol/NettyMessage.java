9c87eb0e821de88d97970a5efee83fe054e239a5
majf <majf.colt@gmail.com>
2015-10-13 17:17:33 +0800

    增加私有协议对象

---
+++ src/main/java/com/coltcn/chatserver/protocol/NettyMessage.java
@@ -0,0 +1,30 @@
+package com.coltcn.chatserver.protocol;
+
+/**
+ * Created by majf on 2015/10/13.
+ */
+public class NettyMessage {
+    private Header header;
+    private Object body;
+
+    public Header getHeader() {
+        return header;
+    }
+
+    public void setHeader(Header header) {
+        this.header = header;
+    }
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return "NettyMessage [header="+header+"]";
+    }
+}
